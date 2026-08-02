6c55ea2a6b1d710656f690ffeb72fd12c0b0ee0a
unknown <karitakors@gmail.com>
2026-08-02 15:10:27 +0300

    unicafe6

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -2,7 +2,10 @@ import { useState } from 'react'
 
 const StatisticsLine = (props) => {
   return (
-    <p>{props.text} {props.value}</p>
+    <tr>
+      <td>{props.text}</td>
+      <td>{props.value}</td>
+    </tr>
   )
 }
 
@@ -27,13 +30,15 @@ const Statistics = (props) => {
   return (
     <div>
       <h2>statistics</h2>
+      <table>
+        <tbody>
       <StatisticsLine text="good" value={props.good} />
       <StatisticsLine text="neutral" value={props.neutral} />
       <StatisticsLine text="bad" value={props.bad} />
       <StatisticsLine text="all" value={props.total} />
       <StatisticsLine text="average" value={props.average} />
       <StatisticsLine text="positive" value={props.positive + " %"} />
-    </div>
+    </tbody></table></div>
   )
 }
 
