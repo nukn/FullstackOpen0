d8629104539be1fe987a08eb75a989e674c8baae
unknown <karitakors@gmail.com>
2026-08-02 21:23:54 +0300

    2.5

--- osa2/kurssitiedot2/src/App.jsx
+++ osa2/kurssitiedot2/src/App.jsx
@@ -1,47 +1,6 @@
-const Header = (props) => {
-  return (
-    <h1>
-      {props.course.name}
-    </h1>
-  )
-}
 
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
+import Course from './Course'
 
-const Content = (props) => {
-  return (
-    <div>
-      {props.course.parts.map(part => 
-        <Part key={part.name} part={part} />
-      )}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const total = props.course.parts.reduce((sum, part) => sum + part.exercises, 0)
-  return (
-    <div>
-     <b> Total: {total}</b>
-    </div>
-  )
-}
-
-const Course = (props) => {
-  return (
-      <div>
-      <Header course={props.course} />
-      <Content course={props.course} />
-      <Total course={props.course} />
-    </div>
-  )
-}
 
 const App = () => {
 const courses = [
